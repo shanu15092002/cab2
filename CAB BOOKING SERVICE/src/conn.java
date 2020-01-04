5bd183dd8cd9dbea7cd979c41f69d504058d9909
shanu15092002 <59475809+shanu15092002@users.noreply.github.com>
2020-01-04 21:49:58 +0530

    Add files via upload

---
+++ CAB BOOKING SERVICE/src/conn.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SAMEER
+ */
+class conn {
+
+    static void close() {
+        throw new UnsupportedOperationException("");
+    }
+    
+}
---
+++ CAB BOOKING SERVICE/src/stmt.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SAMEER
+ */
+class stmt {
+
+    static void close() {
+        throw new UnsupportedOperationException("");
+    }
+}
+    
+
